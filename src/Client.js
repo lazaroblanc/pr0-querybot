15d65a748ffe3cfbdc824662b0ddd1499cb1df5c
Lázaro Blanc <lazaroblanc@users.noreply.github.com>
2021-10-03 21:48:45 +0200

    moved Client code from index.js to Client.js

--- src/Client.js
+++ src/Client.js
@@ -4,9 +4,18 @@ const path = require('path');
 
 logger.debug(`Called ${path.basename(__filename)}`);
 
-module.exports = new Discord.Client({
+let client = new Discord.Client({
     intents: [Discord.Intents.FLAGS.GUILDS],
     presence: {
         status: 'online'
     }
 });
+
+client.login(process.env.BOT_TOKEN);
+
+client.on('ready', async () => {
+    let numberOfGuilds = client.guilds.cache.size;
+    logger.info(`Bot is running as ${client.user.tag} on ${numberOfGuilds} guild${numberOfGuilds > 1 ? 's' : ''}`);
+});
+
+module.exports = client;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,6 @@ const client = require('./Client');
 const onExit = require('signal-exit');
 require('dotenv').config();
 
-client.login(process.env.BOT_TOKEN);
-
-client.on('ready', async () => {
-    let numberOfGuilds = client.guilds.cache.size;
-    logger.info(`Bot is running as ${client.user.tag} on ${numberOfGuilds} guild${numberOfGuilds > 1 ? 's' : ''}`);
-});
-
 onExit((code, signal) => {
     logger.info(`${signal} received, shutting down!`);
     client.destroy();
