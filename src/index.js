e9e294912e2539429af1183011b739497a16df50
Lázaro Blanc <lazaroblanc@users.noreply.github.com>
2021-10-03 21:56:12 +0200

    Added startup message to index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const logger = require('./Logger');
 const client = require('./Client');
 const onExit = require('signal-exit');
+const fs = require('fs');
+const pjson = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
 require('dotenv').config();
+
+logger.info(`Starting ${pjson.name} v${pjson.version}`);
 
 onExit((code, signal) => {
     logger.info(`${signal} received, shutting down!`);
