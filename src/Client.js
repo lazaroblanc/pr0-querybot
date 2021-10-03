ab911e5d191b07d522fc31873d3e3d033198f296
Lázaro Blanc <lazaroblanc@users.noreply.github.com>
2021-10-03 22:51:20 +0200

    Added debug message to Client.js

--- src/Client.js
+++ src/Client.js
@@ -14,6 +14,7 @@ let client = new Discord.Client({
 client.login(process.env.BOT_TOKEN);
 
 client.on('ready', async () => {
+    logger.debug('Client emitted ready');
     let numberOfGuilds = client.guilds.cache.size;
     logger.info(`Bot is running as ${client.user.tag} on ${numberOfGuilds} guild${numberOfGuilds > 1 ? 's' : ''}`);
 });
